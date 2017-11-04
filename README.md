67a3d456b1db5e85da49216ef90e64fd894290f2
Gwennael Jean <gwennael.jean@gmail.com>
2017-11-05 00:09:08 +0100

    chore: exportation de la base de données

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Compte
+#Compte
+
+## Introduction
+
+Compte est une application de gestion des comptes bancaire. Inserez vos transaction, votre salaire et le tour est joué.
+
+## Installation
+
+ * Paramétrez votre base de données à l'aide des identifiants de connexion MySQL dans le fichier Gestion/getDB.php (ligne 5)
+ * Créez la base de données (par exemple, avec l'aide de phpmyadmin)
+ * Importez le document schema.sql
---
+++ schema.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 05, 2017 at 12:04 AM
+-- Server version: 5.5.57-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `freyss_compte`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resume`
+--
+
+CREATE TABLE IF NOT EXISTS `resume` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `annee` int(11) NOT NULL,
+  `numMois` int(11) NOT NULL,
+  `mois` varchar(100) NOT NULL,
+  `salaire` double NOT NULL,
+  `depense` double NOT NULL,
+  `benefice` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
